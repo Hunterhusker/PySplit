45b6f485d31b9d7fc8671529f6572a3b4b33701d
Hunter <hunterhusker@gmail.com>
2025-03-22 17:53:31 -0500

    improved timer widget

--- Widgets/TimerWidget.py
+++ Widgets/TimerWidget.py
@@ -1,22 +1,40 @@
-from PySide6.QtWidgets import QWidget, QLabel, QSizePolicy, QHBoxLayout
-from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QWidget, QLabel, QSizePolicy, QVBoxLayout, QFrame
+from PySide6.QtCore import Slot, Qt
 
 from helpers.TimerFormat import format_wall_clock_from_ms
 
 
-class TimerWidget(QWidget):
+class TimerWidget(QFrame):
     def __init__(self):
         super().__init__()
 
-        self.layout = QHBoxLayout()
+        self.layout = QVBoxLayout()
 
         self.MainTimerLabel = QLabel("00.000", self)
-        self.MainTimerLabel.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Fixed)
+        self.MainTimerLabel.setObjectName('TimeLabel')
+        self.MainTimerLabel.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
 
+        self.layout.addStretch(0)
         self.layout.addWidget(self.MainTimerLabel)
+        self.layout.addStretch(0)
 
+        self.layout.setAlignment(self.MainTimerLabel, Qt.AlignRight)
+
+        self.setObjectName('TimeFrame')
         self.setStyleSheet("""
-            color: #bbbbbb;
+            #TimeFrame {
+                border-bottom: 1px solid #bbbbbb;
+                border-top: 1px solid #bbbbbb;
+                border-left: none;
+                border-right: none;
+            }
+            
+            #TimeLabel {
+                border-bottom: none;
+                border-top: none;
+                border-left: none;
+                border-right: none;
+            }
         """)
 
         self.setLayout(self.layout)
