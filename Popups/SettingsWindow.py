197651f90299c5e5b1b673ed80df5d3103d805b4
Hunter <hunterhusker@gmail.com>
2025-04-09 20:52:34 -0500

    started figuring out how to do tab containers for the settings

--- Popups/SettingsWindow.py
+++ Popups/SettingsWindow.py
@@ -1,13 +1,35 @@
-from PySide6.QtWidgets import QLabel, QVBoxLayout, QHBoxLayout, QDialog, QDialogButtonBox, QPushButton, QMessageBox, QFrame, QWidget
+from PySide6.QtWidgets import QLabel, QVBoxLayout, QHBoxLayout, QDialog, QDialogButtonBox, QPushButton, QMessageBox, \
+    QFrame, QWidget, QTabWidget
 from PySide6.QtCore import Slot, Signal, Qt
 
 
 class SettingsWindow(QDialog):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent: QWidget = None):
+        super().__init__(parent)
 
+        tabs = QTabWidget()
+        tabs.addTab(TempWidget("One"), "Tab1")
+        tabs.addTab(TempWidget("Two"), "Tab2")
+        tabs.addTab(TempWidget("Three"), "Tab3")
+
+        # store the tabs for later
+        self.tabs = tabs
+
+        self.layout = QHBoxLayout()
+
+        self.layout.addWidget(self.tabs)
+
+        self.setLayout(self.layout)
         self.setWindowTitle('Settings')
 
-        self.mainLayout = QHBoxLayout()
 
-        self.setLayout(self.mainLayout)
+class TempWidget(QWidget):
+    def __init__(self, text: str = '', parent: QWidget = None):
+        super().__init__(parent)
+        self.layout = QVBoxLayout()
+
+        self.lbl = QLabel(text, self)
+
+        self.layout.addWidget(self.lbl)
+
+        self.setLayout(self.layout)
