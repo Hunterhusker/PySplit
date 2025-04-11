cfd9b1b3504ed658b2f8aa46c93c504ea203150c
Hunter <hunterhusker@gmail.com>
2025-04-10 19:11:14 -0500

    started figuring out how to do tab containers for the settings

--- Popups/SettingsWindow.py
+++ Popups/SettingsWindow.py
@@ -1,6 +1,8 @@
-from PySide6.QtWidgets import QLabel, QVBoxLayout, QHBoxLayout, QDialog, QDialogButtonBox, QPushButton, QMessageBox, \
-    QFrame, QWidget, QTabWidget
-from PySide6.QtCore import Slot, Signal, Qt
+from PySide6.QtGui import QPainter
+from PySide6.QtWidgets import QLabel, QVBoxLayout, QHBoxLayout, QDialog, QDialogButtonBox, QPushButton, QMessageBox, QFrame, QWidget, QTabWidget
+from PySide6.QtCharts import QLineSeries, QChart, QChartView
+import numpy
+from PySide6.QtCore import Slot, Signal, Qt, QPointF
 
 
 class SettingsWindow(QDialog):
@@ -10,7 +12,7 @@ class SettingsWindow(QDialog):
         tabs = QTabWidget()
         tabs.addTab(TempWidget("One"), "Tab1")
         tabs.addTab(TempWidget("Two"), "Tab2")
-        tabs.addTab(TempWidget("Three"), "Tab3")
+        tabs.addTab(TempXYWidget(), "Tab3")
 
         # store the tabs for later
         self.tabs = tabs
@@ -31,5 +33,25 @@ class TempWidget(QWidget):
         self.lbl = QLabel(text, self)
 
         self.layout.addWidget(self.lbl)
+
+        self.setLayout(self.layout)
+
+
+class TempXYWidget(QWidget):
+    def __init__(self, parent: QWidget = None):
+        super().__init__(parent)
+        self.layout = QVBoxLayout()
+
+        series = QLineSeries()
+        series.append([QPointF(0, 0), QPointF(1, 1), QPointF(2, 4), QPointF(3, 9), QPointF(4, 16)])
+
+        chart = QChart()
+        chart.addSeries(series)
+        chart.createDefaultAxes()
+
+        self.chart_view = QChartView(chart)
+        self.chart_view.setRenderHint(QPainter.Antialiasing)
+
+        self.layout.addWidget(self.chart_view)
 
         self.setLayout(self.layout)
