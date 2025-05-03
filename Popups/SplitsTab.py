d4d22df7c2d98df523f0e10ef36fb6b802c0eaa3
Hunter <hunterhusker@gmail.com>
2025-05-02 22:24:46 -0500

    added the splits tab

--- Popups/SplitsTab.py
+++ Popups/SplitsTab.py
@@ -1,8 +1,9 @@
-from PySide6.QtWidgets import QLabel, QVBoxLayout, QHBoxLayout, QFrame, QWidget, QLineEdit, QScrollArea
-from PySide6.QtCore import Slot, Qt
+from PySide6.QtWidgets import QLabel, QVBoxLayout, QHBoxLayout, QFrame, QWidget, QLineEdit, QScrollArea, QTimeEdit
+from PySide6.QtCore import Slot, Qt, QTime
 from typing import TYPE_CHECKING
 
 from Popups.ABCSettingTab import ABCSettingTab
+from helpers.TimerFormat import qtime_to_ms
 
 if TYPE_CHECKING:
     from Main import Main
@@ -14,7 +15,41 @@ class SplitsTab(ABCSettingTab):
     """
     def __init__(self, mainWindow: 'Main' = None):
         super().__init__(parent=mainWindow)
-        ...
+        self.layout = QVBoxLayout()
+
+        self.test = SplitLine("test split", 0, 0, self)
+        self.layout.addWidget(self.test)
+
+        self.setLayout(self.layout)
 
     def apply(self):
-        pass
+        print(qtime_to_ms(self.test.bestTimeInput.time()))
+
+
+class SplitLine(QFrame):
+    def __init__(self, splitName, bestTimeMs, goldTimeMs, parent=None):
+        super().__init__(parent=parent)
+
+        self.layout = QHBoxLayout()
+
+        self.bestTimeMs = bestTimeMs
+        self.goldTimeMs = goldTimeMs
+
+        self.splitNameInput = QLineEdit()
+        self.splitNameInput.setText(splitName)
+        self.splitNameInput.setFixedSize(125, 25)
+
+        self.goldTimeInput = QTimeEdit()
+        self.goldTimeInput.setDisplayFormat('hh:mm:ss.zzz')
+        self.goldTimeInput.setTime(QTime(0, 0, 0))
+
+        self.bestTimeInput = QTimeEdit()
+        self.bestTimeInput.setDisplayFormat('hh:mm:ss.zzz')
+        self.bestTimeInput.setTime(QTime(0, 0, 0))
+
+        # add them all in one block
+        self.layout.addWidget(self.splitNameInput)
+        self.layout.addWidget(self.goldTimeInput)
+        self.layout.addWidget(self.bestTimeInput)
+
+        self.setLayout(self.layout)
