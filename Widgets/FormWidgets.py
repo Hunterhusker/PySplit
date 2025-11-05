9daa0d85169135144fb2c8df4aac892e58d15d22
Hunter <hunterhusker@gmail.com>
2025-11-04 21:16:36 -0600

    added a spinnerbox

--- Widgets/FormWidgets.py
+++ Widgets/FormWidgets.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QFrame, QHBoxLayout, QLabel, QLineEdit, QSizePolicy
+from PySide6.QtWidgets import QFrame, QHBoxLayout, QLabel, QLineEdit, QSpinBox, QSizePolicy
 
 
 class LabeledTextEntry(QFrame):
@@ -16,6 +16,31 @@ class LabeledTextEntry(QFrame):
         self.input.setMinimumWidth(225)
         self.input.setFixedHeight(25)
         self.input.setText(original_value)
+        self.input.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+
+        self.layout.addWidget(self.label, stretch=1)
+        self.layout.addWidget(self.input, stretch=1)
+        self.layout.setContentsMargins(10, 0, 10, 0)
+
+        self.setLayout(self.layout)
+        self.setObjectName('SettingLine')
+
+
+class LabeledSpinBox(QFrame):
+    def __init__(self, label: str, original_value: int, parent):
+        super().__init__(parent=parent)
+
+        self.layout = QHBoxLayout()
+
+        self.label = QLabel(label)
+        self.label.setMinimumWidth(125)
+        self.label.setFixedHeight(25)
+        self.label.setObjectName('SettingsLabel')
+
+        self.input = QSpinBox()
+        self.input.setMinimumWidth(225)
+        self.input.setFixedHeight(25)
+        self.input.setValue(original_value)
         self.input.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
 
         self.layout.addWidget(self.label, stretch=1)
