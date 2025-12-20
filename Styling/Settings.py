574f77cdc8d1741ce4fa7c4d72f3ec124acb50d2
hunter <hunterhusker@gmail.com>
2025-12-20 15:14:26 -0600

    working on fixing paths, started on QRC implementation but I have to go

--- Styling/Settings.py
+++ Styling/Settings.py
@@ -1,8 +1,11 @@
 from PySide6.QtCore import QObject, Signal
 import json
-
 from Models.Game import Game
+from pathlib import Path
 from Styling.Style.styleBuilder import StyleBuilder
+
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
 
 class Settings(QObject):
@@ -21,13 +24,13 @@ class Settings(QObject):
             settings_data = f.read()
             self.settings = json.loads(settings_data)
 
-        self.style_path = self.settings['style_path']
-        self.var_path = self.settings['var_path']
-        self.game_path = self.settings['game_path']
+        self.style_path =  str(PROJECT_ROOT / self.settings['style_path'])
+        self.var_path = str(PROJECT_ROOT / self.settings['var_path'])
+        self.game_path = str(PROJECT_ROOT / self.settings['game_path'])
 
         # the configurator has a style builder, since it doesn't need to know how to build the styles, just how configure and pass style updates along to the configured
-        self.style = StyleBuilder(self.settings['style_path'], self.settings['var_path'])
-        self.game = Game.from_json_file(self.settings['game_path'])
+        self.style = StyleBuilder(self.style_path, self.var_path)
+        self.game = Game.from_json_file(self.game_path)
 
     def write_settings(self):
         """
