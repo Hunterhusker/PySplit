05879a6eaadbdc46494b42bcd518e6a5f11186bd
hunter <hunterhusker@gmail.com>
2026-03-15 16:24:09 -0500

    prepping for SQL change

--- Models/Game.py
+++ Models/Game.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 
 import json
 from PySide6.QtCore import Signal, QObject
+from Models.SplitDefinition import SplitDefinition
 
 
 class Game(QObject):
     """A representation of a game and the splits we'd like to track during a run"""
     GameUpdated = Signal(QObject)
 
-    def __init__(self, title: str, sub_title: str, splits: list[Split], lifetime_attempts: int, session_attempts: int, start_offset: float, display_pb: bool = True):
+    def __init__(self, title: str, sub_title: str, splits: list[SplitDefinition], lifetime_attempts: int, session_attempts: int, start_offset: float, display_pb: bool = True):
         super().__init__()
 
         self.title = title
@@ -41,7 +42,7 @@ class Game(QObject):
         # build the splits from the JSON in the game dictionary
         splits = []
         for split in json_dict['splits']:
-            new_split = Split.from_json(split, prev_pb_segment_total_ms, prev_gold_segment_total_ms)
+            new_split = SplitDefinition.from_json(split, prev_pb_segment_total_ms, prev_gold_segment_total_ms)
 
             prev_pb_segment_total_ms = new_split.pb_segment_total_ms
             prev_gold_segment_total_ms = new_split.gold_segment_total_ms
@@ -97,7 +98,7 @@ class Game(QObject):
         # build the splits from the JSON in the game dictionary
         splits = []
         for split in json_dict['splits']:
-            new_split = Split.from_json(split, prev_pb_segment_total_ms, prev_gold_segment_total_ms)
+            new_split = SplitDefinition.from_json(split, prev_pb_segment_total_ms, prev_gold_segment_total_ms)
 
             prev_pb_segment_total_ms = new_split.pb_segment_total_ms
             prev_gold_segment_total_ms = new_split.gold_segment_total_ms
@@ -180,74 +181,3 @@ class Game(QObject):
         self.GameUpdated.emit(self)
 
 
-class Split:
-    """A class that represents a single split in a speedrun"""
-    def __init__(self, split_name: str, pb_time_ms: int, pb_segment_ms: int, gold_segment_ms: int, pb_segment_total_ms: int = 0, gold_segment_total_ms: int = 0):
-        self.split_name = split_name
-        self.pb_time_ms = pb_time_ms
-        self.pb_segment_ms = pb_segment_ms
-        self.gold_segment_ms = gold_segment_ms
-        self.pb_segment_total_ms = pb_segment_total_ms
-        self.gold_segment_total_ms = gold_segment_total_ms
-
-    @classmethod
-    def from_json(cls, json_dict: dict, prev_pb_segment_total_ms: int = 0, prev_gold_segment_total_ms: int = 0):
-        """
-        Makes a single split from the dictionary (aka JSON)
-        Args:
-            json_dict: (dict) the JSON dictionary representing the split
-            prev_pb_segment_total_ms: (int) the total of the previously seen segments pb segment times in ms
-            prev_gold_segment_total_ms: (int) the total of the previously seen segments gold segment times in ms
-
-        Returns:
-            (Split) the split detailed in the JSON object
-        """
-        # track the local segment totals
-        prev_pb_segment_total_ms += json_dict['pb_segment_ms']
-        prev_gold_segment_total_ms += json_dict['gold_segment_ms']
-
-        return cls(json_dict['split_name'], json_dict['pb_time_ms'], json_dict['pb_segment_ms'], json_dict['gold_segment_ms'], prev_pb_segment_total_ms, prev_gold_segment_total_ms)
-
-    @classmethod
-    def from_json_str(cls, json_str: str, prev_pb_segment_total_ms: int = 0, prev_gold_segment_total_ms: int = 0):
-        """
-        Makes a split object from a json string representation
-        Args:
-            json_str: (str) the string representing the split as JSON as a string
-            prev_pb_segment_total_ms: (int) the total of the previously seen segments pb segment times in ms
-            prev_gold_segment_total_ms: (int) the total of the previously seen segments gold segment times in ms
-
-        Returns:
-            (Split) the Split object that the JSON string represents
-        """
-        json_dict = json.loads(json_str)
-        return cls.from_json(json_dict, prev_pb_segment_total_ms, prev_gold_segment_total_ms)
-
-    def __str__(self):
-        """
-        Turns the split object into a JSON string
-        Returns:
-            (str) the JSON object as a string
-        """
-        return self.to_json()
-
-    def to_dict(self):
-        """
-        Makes a dictionary out of the Split
-        Returns:
-            (dict): the dictionary that has all the data for this object
-        """
-        return {
-            'split_name': self.split_name,
-            'pb_time_ms': self.pb_time_ms,
-            'pb_segment_ms': self.pb_segment_ms,
-            'gold_segment_ms': self.gold_segment_ms
-        }
-
-    def to_json(self):
-        """
-        Turns the split object into a JSON string
-        Returns:
-            (str) the JSON object as a string
-        """
-        return json.dumps(self.to_dict(), indent=4)
